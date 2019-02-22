ade0c5e32574ce0993340e02054dae1ce791b534
Jian Liu <onanying@126.com>
2019-02-22 12:24:50 +0800

    修改 Command 为 AbstractCommand

---
+++ src/Command/AbstractCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mix\Console\Command;
+
+use Mix\Core\Bean\AbstractObject;
+
+/**
+ * Class AbstractCommand
+ * @package Mix\Console
+ * @author LIUJIAN <coder.keda@gmail.com>
+ */
+abstract class AbstractCommand extends AbstractObject
+{
+}
