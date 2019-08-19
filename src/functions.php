6f77e7f2b4495e4da44be9f5b5c14a55e748d41c
Jian Liu <onanying@126.com>
2019-08-19 17:40:48 +0800

    移动xgo/xdefer到concurrent库

--- src/functions.php
+++ src/functions.php
@@ -21,22 +21,6 @@ if (!function_exists('context')) {
     }
 }
 
-if (!function_exists('xgo')) {
-    // 创建协程
-    function xgo($function, ...$params)
-    {
-        \Mix\Concurrent\Coroutine::create($function, ...$params);
-    }
-}
-
-if (!function_exists('xdefer')) {
-    // 创建延迟执行
-    function xdefer($function)
-    {
-        return \Mix\Concurrent\Coroutine::defer($function);
-    }
-}
-
 if (!function_exists('println')) {
     // 输出字符串并换行
     function println($strings)
